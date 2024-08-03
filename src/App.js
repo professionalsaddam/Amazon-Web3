8213643eacb7eb23ff40abca0803f85f519329ac
Saddam Hussain <syncorb2022@gmail.com>
2024-08-03 14:34:29 +0530

    Completed Navbar and fetching account on connect click

--- src/App.js
+++ src/App.js
@@ -13,12 +13,12 @@ import Dappazon from './abis/Dappazon.json'
 import config from './config.json'
 
 function App() {
+
+  const [account, setAccount] = useState(null);
   
   const loadBlockchainData = async () => {
 
-    const accounts = await window.ethereum.request({'method' : 'eth_requestAccounts'});
-    const account = ethers.utils.getAddress(accounts[0]);
-    console.log(account);
+    
 
   }
 
@@ -31,6 +31,8 @@ function App() {
   return (
     <div>
 
+      <Navigation account={account} setAccount={setAccount} />
+      
       <h2>Welcome to Dappazon</h2>
 
     </div>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,8 +2,49 @@ import { ethers } from 'ethers';
 
 const Navigation = ({ account, setAccount }) => {
 
+
+    const connectHandler = async () => {
+        const accounts = await window.ethereum.request({'method' : 'eth_requestAccounts'});
+        const account = ethers.utils.getAddress(accounts[0]);
+        console.log(account);
+        setAccount(account)
+    }
+
     return (
         <nav>
+            {/* BRAND */}
+            <div className='nav__brand'>
+                <h1>Dappazon</h1>
+            </div>
+
+            {/* SEARCH */}
+            <input className='nav__search' type='text' />
+
+            {/* CONNECT */}
+            { account ?
+                <button className='nav__connect'>
+                    {account.slice(0,6) + "..." + account.slice(32,36) }
+                </button>
+            :
+                <button 
+                    className='nav__connect'
+                    onClick={connectHandler}
+                >
+                    Connect
+                </button>
+
+            }
+
+            {/* LINKS */}
+            <ul className='nav__links'>
+                <li><a href='#Clothing & Jewellery'>Clothing & Jewellery</a></li>
+                <li><a href='#Electronics & Gadgets'>Electronics & Gadgets</a></li>
+                <li><a href='#Toys & Gaming'>Toys & Gaming</a></li>
+
+            </ul>
+
+
+
 
         </nav>
     );
