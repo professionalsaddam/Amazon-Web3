193914fd6f56c684e80f56c5afbc8e8d7670b599
Saddam Hussain <syncorb2022@gmail.com>
2024-08-03 13:10:54 +0530

    Storing and Listing an Item

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -9,4 +9,51 @@ contract Dappazon {
         // name = "Dappazon"; 
         owner = msg.sender;
     }
+
+    struct Item{
+        uint256 id;
+        string name;
+        string category;
+        string image;
+        uint256 cost;
+        uint256 rating;
+        uint256 stock;
+    }
+
+    mapping(uint256 => Item) public items;
+
+    event List(string name, uint256 cost, uint256 quantity);
+
+
+    //List products
+
+    function list(
+        uint256 _id,
+        string memory _name,
+        string memory _category,
+        string memory _image,
+        uint256 _cost,
+        uint256 _rating,
+        uint256 _stocks
+    )
+    public
+    {
+
+        //Create Item Struct
+        Item memory item = Item(_id, _name, _category, _image, _cost, _rating, _stocks);
+
+
+        //Saving Item struct to blockchain
+        items[_id] = item;
+
+        //Emit an Event
+        emit List(_name, _cost, _stocks);
+
+    }
+
+
+    //Buy Product
+
+
+    //Withdraw Fund
 }
--- test/Dappazon.js
+++ test/Dappazon.js
@@ -4,6 +4,15 @@ const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
+// Global constants for listing an item...
+const ID = 1
+const NAME = "Shoes"
+const CATEGORY = "Clothing"
+const IMAGE = "https://ipfs.io/ipfs/QmTYEboq8raiBs7GTUg2yLXB3PMz6HuBNgNfSZBx5Msztg/shoes.jpg"
+const COST = tokens(1)
+const RATING = 4
+const STOCK = 5
+
 describe("Dappazon", () => {
 
   let dappazon;
@@ -35,6 +44,39 @@ describe("Dappazon", () => {
       expect(owner).to.equal(deployer.address);
     })
 
+
+
+  });
+
+
+  describe("Listing",() => {
+
+    let transaction
+    
+    beforeEach(async () => {
+      transaction = await dappazon.connect(deployer).list(ID,NAME, CATEGORY, IMAGE, COST, RATING, STOCK);
+      await transaction.wait();
+    })
+
+    it("returns item attributes", async () => {
+      
+
+      const item = await dappazon.items(ID);
+      expect(item.id).to.equal(ID);
+      expect(item.name).to.equal(NAME);
+      expect(item.category).to.equal(CATEGORY);
+      expect(item.image).to.equal(IMAGE);
+      expect(item.cost).to.equal(COST);
+      expect(item.rating).to.equal(RATING);
+      expect(item.stock).to.equal(STOCK);
+
+
+    });
+
+
+    it("Emits the List Event", async () => {
+      expect(transaction).to.emit(dappazon,"List");
+    })
 
 
   })
