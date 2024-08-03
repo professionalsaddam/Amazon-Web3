852b3b9bf153adc6b1d87709b46516672e511f79
Saddam Hussain <syncorb2022@gmail.com>
2024-08-03 12:09:17 +0530

    First Automated Testing using Hardhat

--- test/Dappazon.js
+++ test/Dappazon.js
@@ -6,4 +6,11 @@ const tokens = (n) => {
 
 describe("Dappazon", () => {
 
+  it("Has a name", async () => {
+    const Dappazon = await ethers.getContractFactory("Dappazon");
+    dappazon = await Dappazon.deploy();
+    const name = await dappazon.name()
+    expect(name).to.be.equal("Dappazon");
+  })
+
 })
