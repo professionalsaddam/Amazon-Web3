b7071453a85deda9c655963fa3cb2da71da33975
Saddam Hussain <syncorb2022@gmail.com>
2024-08-03 14:11:47 +0530

    Added Hardhat Deployment Functionalities

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -12,6 +12,36 @@ const tokens = (n) => {
 }
 
 async function main() {
+  //Setup Account
+  const [deployer] = await ethers.getSigners();
+
+  //Deploy Dappazon Contract
+  const Dappazon = await hre.ethers.getContractFactory("Dappazon");
+  const dappazon = await Dappazon.deploy();
+  await dappazon.deployed();
+
+  console.log(`Dappazon Successfully Deployed at : ${dappazon.address}\n`);
+
+  //Listing Items
+  for(let i =0; i< items.length; i++)
+  {
+
+    const transaction = await dappazon.connect(deployer).list(
+      items[i].id,
+      items[i].name,
+      items[i].category,
+      items[i].image,
+      tokens(items[i].price),
+      items[i].rating,
+      items[i].stock,
+    )
+
+    await transaction.wait();
+
+    console.log(`Listed Item ${items[i].id} : ${items[i].name}`)
+
+  }
+
 
 }
 
